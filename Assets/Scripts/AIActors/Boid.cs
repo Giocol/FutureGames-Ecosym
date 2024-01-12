ab20f30ab70c6b446171ea2ecceea82f33959877
Jo Colomban <giorgio.colomban@edu.futuregames.nu>
2024-01-12 12:19:17 +0100

    feat: implement boid and skeleton of the boid manager

---
+++ Assets/Scripts/AIActors/Boid.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace AIActors {
+    public class Boid : MonoBehaviour{
+        [SerializeField] private float speed = 2f;
+        [SerializeField] private float viewRadius = 3f;
+
+        public float GetSpeed() {
+            return speed;
+        }
+
+        public Boid[] GetClosestBoids() {
+            Collider[] closestBoidsColliders = Physics.OverlapSphere(this.gameObject.transform.position, viewRadius); //TODO: optimize it with the NonAlloc version
+            Boid[] closestBoids = new Boid[closestBoidsColliders.Length];
+            for(int i = 0; i < closestBoidsColliders.Length; i++) {
+                closestBoids[i] = closestBoidsColliders[i].gameObject.GetComponent<Boid>();
+            }
+            return closestBoids;
+        }
+
+        public void Move(Vector3 direction) {
+            Vector3 velocity = direction * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/BoidsManager.cs
@@ -0,0 +1,34 @@
+﻿using AIActors;
+using System;
+using UnityEngine;
+
+public class BoidsManager : MonoBehaviour{
+    [SerializeField] private int numberOfBoids;
+    private Boid[] boids;
+    private Vector3 direction;
+
+    private void Update() {
+        foreach(Boid boid in boids) {
+            Boid[] context = boid.GetClosestBoids();
+            direction = Vector3.zero;
+
+            direction += Cohesion(boid, context);
+            direction += Separation(boid, context);
+            direction += Alignment(boid, context);
+
+            boid.Move(direction);
+        }
+    }
+
+    private Vector3 Alignment(Boid boid, Boid[] context) {
+        throw new NotImplementedException();
+    }
+
+    private Vector3 Separation(Boid boid, Boid[] context) {
+        throw new NotImplementedException();
+    }
+
+    private Vector3 Cohesion(Boid boid, Boid[] context) {
+        throw new NotImplementedException();
+    }
+}
